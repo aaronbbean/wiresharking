eec0ad89e6abb9029a96229f04846e3f969ffa40
Aaron Bean <71397189+aaronbbean@users.noreply.github.com>
2024-05-08 16:58:28 -0500

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,8 @@
+"""The plan for this code is that it takes to pcap files to only give you the info you want.
+
+also maybe a live scan option to scan agenst a file to find what you want.""" 
+
+start here. https://github.com/KimiNewt/pyshark/
+
+and https://gist.githubusercontent.com/vijayanandrp/7927dcc64c33f42a6432b321806dfafd/raw/400b551c787bfaefa2d32f7695f2c69a79c7b8c3/read_captured_pcap_file_using_python.py
+
